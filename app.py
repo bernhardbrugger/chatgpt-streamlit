6f9bfdacc1b0002ef787da2e29be8ab67e02cdeb
bernhardbrugger <96493137+bernhardbrugger@users.noreply.github.com>
2023-04-12 11:13:18 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,41 +1,25 @@
-import openai
-import toml
 import streamlit as st
-def show_messages(text):
-    messages_str = [
-        f"{_['role']}: {_['content']}" for _ in st.session_state["messages"][1:]
-    ]
-    text.text_area("Messages", value=str("\n".join(messages_str)), height=400)
+import os
+import openai
 
+# Define environmental variables
+os.environ["__ORGANIZATION_ID"] = "org-HgO75LNERg28F4GqsY4AbfzW"
+os.environ["__API_KEY"] = "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"
 
-with open("secrets.toml", "r") as f:
-    config = toml.load(f)
+# Credentials
+openai.organization = os.getenv("__ORGANIZATION_ID")
+openai.api_key = os.getenv("__API_KEY")
 
-openai.api_key = config["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"]
-BASE_PROMPT = [{"role": "system", "content": "You are a helpful assistant."}]
-
-if "messages" not in st.session_state:
-    st.session_state["messages"] = BASE_PROMPT
-
-st.header("STREAMLIT GPT-3 CHATBOT")
-
-text = st.empty()
-show_messages(text)
-
-prompt = st.text_input("Prompt", value="Enter your message here...")
+# Streamlit app layout
+st.title("Chat with GPT-4")
+user_message = st.text_input("Enter your message:")
 
 if st.button("Send"):
-    with st.spinner("Generating response..."):
-        st.session_state["messages"] += [{"role": "user", "content": prompt}]
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo", messages=st.session_state["messages"]
-        )
-        message_response = response["choices"][0]["message"]["content"]
-        st.session_state["messages"] += [
-            {"role": "system", "content": message_response}
+    completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": user_message}
         ]
-        show_messages(text)
+    )
 
-if st.button("Clear"):
-    st.session_state["messages"] = BASE_PROMPT
-    show_messages(text)
+    st.write(completion.choices[0].message["content"])
